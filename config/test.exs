11ae6ae39ac8301c99742a383220606da2c5c929
Tyrone Taylor <28680107+ttaylor92@users.noreply.github.com>
2024-07-29 21:17:59 -0500

    updated test config

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :payment_server, PaymentServer.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   hostname: "localhost",
   database: "payment_server_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
